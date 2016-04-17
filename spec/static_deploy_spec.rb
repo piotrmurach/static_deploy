55f9d801786c58d40e2451089ac52d91ee08ccd9
Piotr Murach <pmurach@gmail.com>
2016-04-17 19:35:38 +0100

    Remove nonsense.

--- spec/static_deploy_spec.rb
+++ spec/static_deploy_spec.rb
@@ -4,8 +4,4 @@ describe StaticDeploy do
   it 'should have a version number' do
     StaticDeploy::VERSION.should_not be_nil
   end
-
-  it 'should do something useful' do
-    false.should be_true
-  end
 end
